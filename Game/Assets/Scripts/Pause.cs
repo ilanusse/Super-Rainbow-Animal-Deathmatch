d08104eb9e389c547cbf3b1e7d0d192c1eb0ef7c
Kaz <inakilanusse@gmail.com>
2014-09-10 01:21:53 -0300

    Pause and better mobility

---
+++ Game/Assets/Scripts/Pause.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	private bool paused = false;
+	public KeyCode pauseKey = KeyCode.Escape;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (pauseKey)) {
+			if(paused) {
+				Time.timeScale = 1;
+				paused = false;
+			} else {
+				Time.timeScale = 0;
+				paused = true;
+			}
+		}
+	}
+}
--- Game/Assets/Scripts/PlayerMobility.cs
+++ Game/Assets/Scripts/PlayerMobility.cs
@@ -15,13 +15,16 @@ public class PlayerMobility : MonoBehaviour {
 	void FixedUpdate() {
 		Vector3 vel = transform.forward * speed;
 		rigidbody.angularVelocity = new Vector3 (0, 0, 0);
+		rigidbody.rotation = new Quaternion (0f, rigidbody.rotation.y, rigidbody.rotation.z, rigidbody.rotation.w);
 		rigidbody.velocity = new Vector3 (vel.x, rigidbody.velocity.y, vel.z);
 		
 		if (Input.GetKey (leftKey)) {
 			transform.Rotate (-Vector3.up * turnSpeed * Time.deltaTime);
-		} else if (Input.GetKey (rightKey)) {
+		} 
+		if (Input.GetKey (rightKey)) {
 			transform.Rotate (Vector3.up * turnSpeed * Time.deltaTime);
-		} else if (Input.GetKey (jumpKey)) {
+		}
+		if (Input.GetKey (jumpKey)) {
 			if(!midair) {
 				rigidbody.AddForce(new Vector3(0f, jumpForce, 0f));
 				midair = true;
